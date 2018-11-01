9205516ac90b3f3e277471fecb594188d0b0d7fb
Vyacheslav <mister.slaus@gmail.com>
2018-11-01 08:51:00 +0200

    Add files via upload

---
+++ app/js/script.js
@@ -0,0 +1,198 @@
+//Scripts
+//Dropdown multilevel menu
+$('.dropdown-menu a.dropdown-toggle').on('click hover', function(e) {
+    if (!$(this).next().hasClass('show')) {
+        $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+    }
+    var $subMenu = $(this).next(".dropdown-menu");
+    $subMenu.toggleClass('show');
+
+
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+        $('.dropdown-submenu .show').removeClass("show");
+    });
+
+
+    return false;
+});
+
+//Back to top button
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 400) {
+        $('#back-to-top').fadeIn();
+    } else {
+        $('#back-to-top').fadeOut();
+    }
+});
+// scroll body to 0px on click
+$('#back-to-top').click(function () {
+    $('#back-to-top').tooltip('hide');
+    $('body,html').animate({
+        scrollTop: 0
+    }, 800);
+    return false;
+});
+
+//Carousel responsive
+$(function () {
+
+    var jcarousel = $('.jcarousel'),
+        jcarouselGallery = $('.jcarousel.gallery'),
+        jcarouselReviews = $('.jcarousel.reviews');
+
+    jcarousel
+        .on('jcarousel:reload jcarousel:create', function () {
+            var carousel = $(this),
+                width = carousel.innerWidth();
+
+            if (width >= 960) {
+                width = width / 3;
+            } else if (width >= 720) {
+                width = width / 2;
+            } else if (width >= 540) {
+                width = width / 1;
+            } else if (width > 300) {
+                width = width;
+            }
+
+            carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+        })
+        .jcarousel({
+            wrap: 'circular',
+            animation: {
+                duration: 1000,
+                speed: 1000,
+                easing: 'linear',
+                complete: function () {
+                }
+            }
+        })
+        .jcarouselAutoscroll({
+            interval: 8000,
+            target: '+=1',
+            autostart: true,
+        })
+        .on('mouseover', function (e) {
+            $(this).jcarouselAutoscroll('stop');
+        })
+        .on('mouseout', function (e) {
+            $(this).jcarouselAutoscroll('start');
+        });
+
+    jcarouselReviews
+        .on('jcarousel:reload jcarousel:create', function () {
+            var carousel = $(this),
+                width = carousel.innerWidth();
+
+            if (width >= 960) {
+                width = width / 3;
+            } else if (width >= 720) {
+                width = width / 2;
+            } else if (width >= 540) {
+                width = width / 1;
+            } else if (width > 300) {
+                width = width;
+            }
+
+            carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+        })
+        .jcarousel({
+            wrap: 'circular',
+            animation: {
+                duration: 1000,
+                speed: 1000,
+                easing: 'linear',
+                complete: function () {
+                }
+            }
+        })
+        .jcarouselAutoscroll({
+            interval: 8000,
+            target: '+=1',
+            autostart: true,
+        })
+        .on('mouseover', function (e) {
+            $(this).jcarouselAutoscroll('stop');
+        })
+        .on('mouseout', function (e) {
+            $(this).jcarouselAutoscroll('start');
+        });
+
+
+
+    jcarouselGallery
+        .on('jcarousel:reload jcarousel:create', function () {
+            var carousel = $(this),
+                width = carousel.innerWidth();
+
+            if (width >= 960) {
+                width = width / 4;
+            } else if (width >= 720) {
+                width = width / 3;
+            } else if (width >= 540) {
+                width = width / 2;
+            } else if (width > 300) {
+                width = width;
+            }
+
+            carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+        })
+        .jcarousel({
+            wrap: 'circular',
+            animation: {
+                duration: 1000,
+                speed: 1000,
+                easing: 'linear',
+                complete: function () {
+                }
+            }
+        })
+        .jcarouselAutoscroll({
+            interval: 8000,
+            target: '+=1',
+            autostart: true,
+        })
+        .on('mouseover', function (e) {
+            $(this).jcarouselAutoscroll('stop');
+        })
+        .on('mouseout', function (e) {
+            $(this).jcarouselAutoscroll('start');
+        });
+
+    $('.jcarousel-control-prev')
+        .jcarouselControl({
+            target: '-=1'
+        });
+
+    $('.jcarousel-control-next')
+        .jcarouselControl({
+            target: '+=1'
+        });
+
+    $('.jcarousel-pagination')
+        .on('jcarouselpagination:active', 'a', function () {
+            $(this).addClass('active');
+        })
+        .on('jcarouselpagination:inactive', 'a', function () {
+            $(this).removeClass('active');
+        })
+        .on('click', function (e) {
+            e.preventDefault();
+        })
+        .jcarouselPagination({
+            perPage: 1,
+            item: function (page) {
+                return '<a href="#' + page + '">' + page + '</a>';
+            }
+        });
+
+//Match Height
+    $(function() {
+        $('.item').matchHeight({
+            byRow: true,
+            property: 'height',
+            target: null,
+            remove: false
+        });
+    });
+});
